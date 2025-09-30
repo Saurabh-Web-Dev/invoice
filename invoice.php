a7cf85401b9a1f2721e15e8e48e5022f3156a33e
unknown <Saurabh-Web-Dev>
2025-09-30 15:01:24 +0530

    Fixed Invoice Product Name Warp issue

--- invoice.php
+++ invoice.php
@@ -276,51 +276,128 @@ class invoicr extends FPDF_rotation
 
         if ($this->items) {
             foreach ($this->items as $item) {
-                $this->SetFont('helvetica', 'B', 8);
+                $this->SetFont('helvetica', 'b', 8);
                 $this->SetTextColor(50, 50, 50);
                 $this->SetFillColor($bgcolor, $bgcolor, $bgcolor);
                 $this->Cell(1, $cellHeight, '', 0, 0, 'L', 1);
-                $this->Cell($this->firstColumnWidth, $cellHeight, iconv("UTF-8", "ISO-8859-1", $item['item']), 0, 0, 'L', 1);
+                // Save current X and Y
+				$x = $this->GetX();
+				$y = $this->GetY();
 
-                $this->Cell($this->columnSpacing, $cellHeight, '', 0, 0);
-                $this->Cell($width_other, $cellHeight, $item['quantity'], 0, 0, 'C', 1);
-                $this->Cell($this->columnSpacing, $cellHeight, '', 0, 0);
-                $this->Cell($width_other, $cellHeight, $item['vat'], 0, 0, 'C', 1);
-                $this->Cell($this->columnSpacing, $cellHeight, '', 0, 0);
-                $this->Cell($width_other, $cellHeight, $this->currency . ' ' . number_format($item['price'], 2, $this->referenceformat[0], $this->referenceformat[1]), 0, 0, 'C', 1);
+				// Product name with wrapping
+				$this->MultiCell($this->firstColumnWidth, $cellHeight, iconv("UTF-8", "ISO-8859-1", $item['item']), 0, 'L', 1);
 
-                if (isset($this->discountField)) {
-                    $this->Cell($this->columnSpacing, $cellHeight, '', 0, 0);
-                    $this->Cell($width_other, $cellHeight, $item['discount'] ?? '', 0, 0, 'C', 1);
-                }
+				// Move back to the right of the first column for the other columns
+				$this->SetXY($x + $this->firstColumnWidth + $this->columnSpacing, $y);
 
-                $this->Cell($this->columnSpacing, $cellHeight, '', 0, 0);
-                $this->Cell($width_other, $cellHeight, $this->currency . ' ' . number_format($item['total'], 2, $this->referenceformat[0], $this->referenceformat[1]), 0, 0, 'C', 1);
+				// Then print the rest of the row (amount, VAT, price, etc.) as usual
+				$this->Cell($width_other, $cellHeight, $item['quantity'], 0, 0, 'C', 1);
+				$this->Cell($this->columnSpacing, $cellHeight, '', 0, 0);
+				$this->Cell($width_other, $cellHeight, $item['vat'], 0, 0, 'C', 1);
+				$this->Cell($this->columnSpacing, $cellHeight, '', 0, 0);
+				$this->Cell($width_other, $cellHeight, $this->currency . ' ' . number_format($item['price'], 2, $this->referenceformat[0], $this->referenceformat[1]), 0, 0, 'C', 1);
+
+				if (isset($this->discountField)) {
+					$this->Cell($this->columnSpacing, $cellHeight, '', 0, 0);
+					$this->Cell($width_other, $cellHeight, $item['discount'] ?? '', 0, 0, 'C', 1);
+				}
+
+				$this->Cell($this->columnSpacing, $cellHeight, '', 0, 0);
+				$this->Cell($this->columnSpacing, $cellHeight, '', 0, 0);
+                $totalValue = is_numeric($item['total']) ? (float)$item['total'] : 0;
+				$this->Cell(
+					$width_other,
+					$cellHeight,
+					$this->currency . ' ' . number_format($totalValue, 2, $this->referenceformat[0], $this->referenceformat[1]),
+					0,
+					0,
+					'C',
+					1
+				);
+
+				// Move to next line
+				$this->Ln($cellHeight + $this->columnSpacing);
+
+
+                // if (isset($this->discountField)) {
+                //     $this->Cell($this->columnSpacing, $cellHeight, '', 0, 0);
+                //     $this->Cell($width_other, $cellHeight, $item['discount'] ?? '', 0, 0, 'C', 1);
+                // }
+
+                
+
 
                 $this->Ln();
                 $this->Ln($this->columnSpacing);
             }
         }
+		$badgeX = $this->getX();
+		$badgeY = $this->getY();
 
-        // ✅ Totals
-        if ($this->totals) {
-            foreach ($this->totals as $total) {
-                $this->SetFont($this->font, 'b', 8);
-                if ($total['colored']) {
-                    $this->SetTextColor(255, 255, 255);
-                    $this->SetFillColor($this->color[0], $this->color[1], $this->color[2]);
-                } else {
-                    $this->SetTextColor(50, 50, 50);
-                    $this->SetFillColor($bgcolor, $bgcolor, $bgcolor);
-                }
-                $this->Cell(1 + $this->firstColumnWidth, $cellHeight, '', 0, 0, 'L', 0);
-                $this->Cell($width_other - 1, $cellHeight, $total['name'], 0, 0, 'L', 1);
-                $this->Cell($this->columnSpacing, $cellHeight, '', 0, 0);
-                $this->Cell($width_other, $cellHeight, $total['value'], 0, 0, 'C', 1);
-                $this->Ln();
-                $this->Ln($this->columnSpacing);
-            }
-        }
+        //Add totals
+		if($this->totals) 
+		{
+			foreach($this->totals as $total) 
+			{
+				$this->SetTextColor(50,50,50);
+				$this->SetFillColor($bgcolor,$bgcolor,$bgcolor);
+				$this->Cell(1+$this->firstColumnWidth,$cellHeight,'',0,0,'L',0);
+				for($i=0;$i<$this->columns-3;$i++) 
+				{
+					$this->Cell($width_other,$cellHeight,'',0,0,'L',0);
+					$this->Cell($this->columnSpacing,$cellHeight,'',0,0,'L',0);
+				}
+				$this->Cell($this->columnSpacing,$cellHeight,'',0,0,'L',0);
+				if($total['colored']) 
+				{
+					$this->SetTextColor(255,255,255);
+					$this->SetFillColor($this->color[0],$this->color[1],$this->color[2]);
+				}
+				$this->SetFont($this->font,'b',8);
+				$this->Cell(1,$cellHeight,'',0,0,'L',1);
+				$this->Cell($width_other-1,$cellHeight,iconv('UTF-8', 'windows-1252',$total['name']),0,0,'L',1);
+				$this->Cell($this->columnSpacing,$cellHeight,'',0,0,'L',0);
+				$this->SetFont($this->font,'b',8);
+				$this->SetFillColor($bgcolor,$bgcolor,$bgcolor);
+				if($total['colored']) 
+				{
+					$this->SetTextColor(255,255,255);
+					$this->SetFillColor($this->color[0],$this->color[1],$this->color[2]);
+				}
+				$this->Cell($width_other,$cellHeight,iconv('UTF-8', 'windows-1252',$total['value']),0,0,'C',1);
+				$this->Ln();
+				$this->Ln($this->columnSpacing);
+			}
+		}
+		$this->productsEnded = true;
+		$this->Ln();
+		$this->Ln(3);
+		
+		
+		//Badge
+		if($this->badge) 
+		{
+			$badge = ' '.strtoupper($this->badge).' ';
+			$resetX = $this->getX();
+			$resetY = $this->getY();
+			$this->setXY($badgeX,$badgeY+15);
+			$this->SetLineWidth(0.4);
+			$this->SetDrawColor($this->color[0],$this->color[1],$this->color[2]);		
+			$this->setTextColor($this->color[0],$this->color[1],$this->color[2]);
+			$this->SetFont($this->font,'b',15);
+			$this->Rotate(10,$this->getX(),$this->getY());
+			$this->Rect($this->GetX(),$this->GetY(),$this->GetStringWidth($badge)+2,10);
+			$this->Write(10,$badge);
+			$this->Rotate(0);
+			if($resetY>$this->getY()+20) 
+			{
+				$this->setXY($resetX,$resetY);
+			} 
+			else 
+			{
+				$this->Ln(18);
+			}
+		}
 
         // ✅ Extra info text (notes, etc.)
         foreach ($this->addText as $text) {
